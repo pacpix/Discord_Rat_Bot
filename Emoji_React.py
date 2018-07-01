18ea47a3129e014e25fb01b88c07e8432aa20bc1
pacpix <stachofskyjustin@gmail.com>
2018-07-01 12:20:11 -0700

    Added user requests

--- Emoji_React.py
+++ Emoji_React.py
@@ -107,6 +107,14 @@ def on_message(message):
     elif 'ethereum' in message.content:
         msg = '>ethereum. lmao nice meme'.format(message)
         yield from client.send_message(message.channel, msg, tts=True)
+    elif 'shit' in message.content:
+        msg = discord.embed()
+        msg.set_image(url="https://i.imgur.com/E4MMD53.jpg")
+        yield from client.send_message(message.channel, embed=msg)
+    elif 'E' in message.content:
+        msg = discord.embed()
+        msg.set_image(url="https://i.imgur.com/paScsKo.png")
+        yield from client.send_message(message.channel, embed=msg)
 
 @client.event
 @asyncio.coroutine
